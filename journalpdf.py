031a020ccc89d0651a761ec677f2af53a0e6e3f7
Paul Lee <paul@strivenword.com>
2026-04-08 11:44:36 -0400

    Fixed syntax error.

--- journalpdf.py
+++ journalpdf.py
@@ -620,8 +620,12 @@ class PdfConverter:
 
     def detect_heading_candidates(self, blocks):
         for index, block in enumerate(blocks):
-            if block.type not in (BlockType.PARAGRAPH, BlockType.HEADING, BlockType.REFERENCE_HEADING):
+            if block.type not in (
+                BlockType.PARAGRAPH, BlockType.HEADING, BlockType.REFERENCE_HEADING
+                ):
                 continue
+                if any(line["line_type"] == LineType.FOOTNOTE for line in block.lines):
+                    continue
             if block.type is BlockType.REFERENCE_HEADING:
                 block.heading_candidate = HeadingCandidate(block.text, 1.0, level_hint=1)
                 continue
